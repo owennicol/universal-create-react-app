33d720460bc84828814dfd743afe7078f0406c36
Owen Nicol <owennicol@gmail.com>
2017-10-10 19:47:02 +0100

    Viewport changes

--- src/server/render.js
+++ src/server/render.js
@@ -14,7 +14,7 @@ export default (component) => `
     <html lang="en">
       <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
         <meta name="theme-color" content="#000000">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
         ${css}
