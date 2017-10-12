4dcc5caa5f88a30e568c2b16324d9b8796b78dd2
Owen Nicol <owennicol@gmail.com>
2017-10-12 13:45:20 +0100

    Delete in basket

--- src/shared/components/Basket.js
+++ src/shared/components/Basket.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { deleteFromBasket } from '../actions/basket'
 
 const Basket = (props) => {
   if (!props.basket.length) return null
   return (
     <div className='basket'>
       <h4>Basket</h4>
-      {props.basket.map((item, key) => (<p key={`basket-item-${item.id}-${item.name}`}>{item.name}</p>))}
+      {props.basket.map((item, key) => {
+        return (
+          <div className='basket-item' key={`basket-item-${item.id}-${item.name}`}>
+            <span className='basket-name'>{item.name}</span>
+            <button type='button' className='close text-danger' aria-label='Close' onClick={() => props.deleteFromBasket(item)}>
+              <span aria-hidden='true'>&times;</span>
+            </button>
+          </div>
+        )
+      })
+      }
     </div>
   )
 }
 
 function mapStateToProps (state, ownProps) {
-  return { basket: state.basket }
+  return {
+    basket: state.basket
+  }
 }
 
 function mapDispatchToProps (dispatch, ownProps) {
-  return {}
+  return {
+    deleteFromBasket: (user) => dispatch(deleteFromBasket(user))
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Basket)
